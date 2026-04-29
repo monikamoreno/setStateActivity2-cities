db466da8564456ad0ad1fe9acea9ccc23c00a501
Monika Moreno <92850379+monikamoreno@users.noreply.github.com>
2026-04-29 11:26:59 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>My favourite cities</h1>
+      <ul>
+        <li>Sevilla in Spring</li>
+        <li>Cracow in Summer</li>
+        <li>New York in Winter</li>
+      </ul>
+      <button>Change cities</button>
+    </div>
+  );
+}
+
+export default App;
